584a2ada5cf9a2bde41187a10a673ae54cefce39
hwyee <hwyee@foxmail.com>
2025-04-08 18:04:27 +0800

    init demo.

---
+++ chat/src/main/java/cn/hwyee/chat/controller/DeepSeekController.java
@@ -0,0 +1,45 @@
+package cn.hwyee.chat.controller;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+/**
+ * @author hui
+ * @version 1.0
+ * @className DeepSeekController
+ * @description
+ * @date 2025/4/8
+ * @since JDK 1.8
+ */
+@RestController
+@RequestMapping("/deepseek")
+public class DeepSeekController {
+
+    private final OpenAiChatModel chatModel;
+
+    @Autowired
+    public DeepSeekController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+}
